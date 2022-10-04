30c5e4e9cec34704c6b826e43f71fecf8a3da8da
Jeffrey <gene8246@gmail.com>
2022-10-04 23:32:29 +0900

    add words count

--- read.py
+++ read.py
@@ -7,6 +7,7 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: #求餘數
 			print(len(data))
 print('loading reviews finished, we have', len(data), 'reviews')
+print(data[0])
 
 sum_len = 0
 for d in data:
@@ -22,6 +23,31 @@ print('review of less than 100 words:', len(new))
 
 good = [d for d in data if 'good' in d]
 print(len(good))
+
+#文字計數
+wc = {} # words_counts
+for d in data:
+	words = d.split() #split預設值為切割空白,且不會對連續空白鍵切割
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 10000:
+		print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('please enter the word you would like to look for: ')
+	if word == 'q':
+		break
+	elif word in wc:
+		print(word, 'has been writen in', wc[word], 'times')
+	else:
+		print(word, 'is not in all reviewes' ) 
 
 #good = []
 #for d in data:
